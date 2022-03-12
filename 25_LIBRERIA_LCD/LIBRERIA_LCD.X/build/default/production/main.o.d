9946aef1a131cc73fc0b7eeb7c74389f6e3ee26d
Diego Suarez <1412612@utp.edu.pe>
2022-03-11 20:51:39 -0500

    enviando libreria LCD

---
+++ 25_LIBRERIA_LCD/LIBRERIA_LCD.X/build/default/production/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/main.o:  \
+	D:/Archivos/Programacion/Microcontroladores/PIC/ASSEMBLER/MPASM/PIC18F26K22/25_LIBRERIA_LCD/LIBRERIA_LCD.X/main.asm  \
+	D:/Archivos/Programacion/Microcontroladores/PIC/ASSEMBLER/MPASM/PIC18F26K22/25_LIBRERIA_LCD/LIBRERIA_LCD.X/LCD4BITS_LIB.inc  \
+	D:/Archivos/Programacion/Microcontroladores/PIC/ASSEMBLER/MPASM/PIC18F26K22/25_LIBRERIA_LCD/LIBRERIA_LCD.X/MY_RETARDOS.inc  \
+
